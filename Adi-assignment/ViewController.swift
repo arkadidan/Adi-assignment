41ee5ad67a1c79e3526db3a59dc3f4e62dfce623
arkadi.daniyelian <arkadzi.daniyelian@accenture.com>
2022-08-11 06:43:09 +0300

    initial

---
+++ Adi-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Adi-assignment
+//
+//  Created by arkadi.daniyelian on 11/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
